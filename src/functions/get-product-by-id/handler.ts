2255e4743c2cca434b90bfed7bbec66f676af1e4
Cuong Nguyen <cuong.ngman@gmail.com>
2023-10-11 17:56:27 +0700

    feat: trigger error callback

--- src/functions/get-product-by-id/handler.ts
+++ src/functions/get-product-by-id/handler.ts
@@ -1,18 +1,18 @@
-import { EventAPIGatewayProxyEvent, formatJSONResponse } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
 import { PRODUCTS } from '@mocks';
 
-const getProductById: EventAPIGatewayProxyEvent = async (event) => {
+const getProductById = async (event, _context, callback) => {
   const { productId } = event.pathParameters;
   const product = PRODUCTS.find((p) => p.id === productId);
-  const statusCode = product ? 200 : 404;
 
-  return formatJSONResponse(
-    {
-      data: product,
-      message: product ? 'Found' : 'Product not found',
-    },
-    statusCode
-  );
+  if (!product) {
+    return callback(new Error('[404] Not Found'));
+  }
+
+  return formatJSONResponse({
+    data: product,
+    message: product ? 'Found' : 'Product not found',
+  });
 };
 
 export const main = getProductById;
--- src/functions/get-products-list/handler.ts
+++ src/functions/get-products-list/handler.ts
@@ -1,8 +1,7 @@
 import { EventAPIGatewayProxyEvent, formatJSONResponse } from '@libs/api-gateway';
 import { PRODUCTS } from '@mocks';
 
-const getProductList: EventAPIGatewayProxyEvent = async (event) => {
-  console.log(event);
+const getProductList: EventAPIGatewayProxyEvent = async (_event, _context, _callback) => {
   return formatJSONResponse({
     data: PRODUCTS,
     counts: PRODUCTS.length,
